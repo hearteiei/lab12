c4520405223433cef2ebac1740076d049d6dbc4e
hearteiei <supalit1@gmail.com>
2021-01-19 17:48:28 +0700

    lab12.2 v.completed

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -21,3 +21,39 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+void showImage(const bool image[][M]){
+    for (int i = 0; i < 32; i++)
+    {
+        for (int j = 0; j < 72; j++)
+        {
+            if (i==0||i==31)
+            {
+                cout << "-";
+            }else if (j==0||j==71)
+            {
+                cout << "|";
+            }else if (i>=1&&j>=1)
+            {
+                if (image[i-1][j-1])
+                {
+                    cout << "*";
+                }else{
+                cout << " ";
+            }
+            }else{
+                cout << " ";
+            }
+
+        }
+        cout <<"\n";
+    }
+}
+    void updateImage(bool image[][M],int s,int x,int y){
+    for (int i = 0; i < 30; i++){
+        for (int j = 0; j < 70; j++){
+        if(pow((pow(i-x,2) + pow(j-y,2)),0.5) <= s-1){
+            image[i][j]=1;           
+        }
+       }
+    }
+}
