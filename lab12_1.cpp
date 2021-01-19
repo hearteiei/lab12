db2a6856977f00694afe8fdb06082ce93fb4e5c3
hearteiei <supalit1@gmail.com>
2021-01-19 17:47:56 +0700

    lab12.1 v.completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -17,3 +17,37 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+void stat(const double score[],int f,double point[]){
+    for(int i =0;i<f;i++){
+        point[0]+=score[i];
+    }
+    point[0]=point[0]/f;
+    for(int i =0;i<f;i++){
+        point[1]+=pow(score[i]-point[0],2);
+    }
+    point[1]=sqrt((point[1]/f));
+    point[2]=1;
+    for(int i =0;i<f;i++){
+        point[2]*=score[i];}
+      double k =f;
+        point[2]=pow(point[2],1/k);
+    for(int i =0;i<f;i++){
+        point[3]+=(1/score[i]);
+    }
+     point[3]=f/point[3];
+      double max =score[0];
+    for(int i =1;i<f;i++){
+        if(score[i]>max){
+            max=score[i];
+            point[4]=score[i];
+        }
+    }
+    double min =score[0];
+     for(int i =1;i<f;i++){
+        if(score[i]<min){
+           min=score[i];
+           point[5]=score[i];
+        }
+    }
+        
+}
